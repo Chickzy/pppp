08146c32fe8fd6506edac157d0ad1188e4e3800d
Chickzy <85591802+Chickzy@users.noreply.github.com>
2021-07-31 14:55:57 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,89 @@
+//Create a reference for canvas 
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
+//Give specific height and width to the car image
+
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
+
+//Set initial position for a car image.
+greencar_x = 5;
+greencar_y = 225;
+function add() {
+	//upload car, and background images on the canvas.
+	background_imgtag = new Image();
+	background_imgTag.onload = uploadBackground;
+	background_imgTag.src = background_image;
+
+	greencar_imgTag = new Image();
+	greencar_imgTag.onload = uploadgreencar;
+	greencar_imgTag.src = greencar_image;
+}
+
+function uploadBackground() {
+	//Define function ‘uploadBackground’
+ctx.drawImage(greencar_imgTag, greencar_x, greencar_y, greencar_width, greencar_height);
+}
+
+function uploadgreencar() {
+	//Define function ‘uploadgreencar’.
+greencar_y = greencar_y - 10;
+console.log("when up arrow is pressed, x=" + greencar_x + " | y =" + greencar_y);
+uploadBackground();
+uploadgreencar();
+	
+}
+
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
+
+function up()
+{
+	//Define function to move the car upward
+}
+
+function down()
+{
+	//Define function to move the car downward
+}
+
+function left()
+{
+	//Define function to move the car left side
+}
+
+function right()
+{
+	//Define function to move the car right side
+}
